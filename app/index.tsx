74b442f05d4d6a11ee8515db275825809aa8a993
Eric Pereira <ericpereira234@gmail.com>
2024-10-24 07:53:04 -0300

    criando a tela do dólar

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,29 @@
-import {View, Text } from "react-native";
+import {View, Text, Image, StyleSheet } from "react-native";
 
 
 export default function Screen(){
     return(
-        <View>
-            <Text>Tela inicial</Text>
+        <View style={styles.container} >
+            <Image source={require('../assets/dolar.png')}
+                resizeMode="contain"
+                style={styles.logo}
+            />
+            <Text>O dólar Americano está:</Text>
+            <Text>R$: 99,99</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#0b1c2d',
+        paddingHorizontal: 20
+    },
+    logo: {
+        width: 200,
+        height: 180
+    }
+})
